329dca01d0f4c86e29f83017de73762dbb8e5c4d
Sakshi Das <sakshidas83498@gmail.com>
2025-12-06 01:17:10 +0530

    initial commit

---
+++ src/app/modals/confirm-modal/confirm-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirm-modal',
+  imports: [],
+  templateUrl: './confirm-modal.html',
+  styleUrl: './confirm-modal.scss',
+})
+export class ConfirmModal {
+
+}
---
+++ src/app/shared/components/star-rating/star-rating.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  imports: [],
+  templateUrl: './star-rating.html',
+  styleUrl: './star-rating.scss',
+})
+export class StarRating {
+
+}
